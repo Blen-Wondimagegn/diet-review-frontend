14a7b5cb247bb112ef7d7511a7153f6018786033
blen_meku <mekuriablen@gmail.com>
2021-01-20 17:29:01 -0800

    set initial state for the attrbuites

---
+++ src/containers/DietNew.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+export class DietNew extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            age: '',
+            gender: '',
+            height: '',
+            diet_type: '',
+            start_weight: '',
+            lost_weight: '',
+            duration: ''
+        }
+    }
+
+    
+
+    render() {
+        return (
+          <div>
+            </div>
+        )
+    }
+}
+export default DietNew;
