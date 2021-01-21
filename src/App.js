7665d130bcda3fb1a3ee99d84573113fb3d96621
blen_meku <mekuriablen@gmail.com>
2021-01-20 17:21:38 -0800

    change App into class componet and import all the child componets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import DietList from './containers/DietList';
+import Likes from './components/Likes';
+import Home from './containers/Home';
+import DietNew from './containers/DietNew.js';
+import DietEdit from './components/DietEdit.js';
+import NavBar from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return (
+    <div>
+      
     </div>
-  );
+    );
+  }
 }
 
 export default App;
