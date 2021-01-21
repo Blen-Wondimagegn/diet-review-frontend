917c74e10e3be7c22152bc1db6d26d440d204fa2
blen_meku <mekuriablen@gmail.com>
2021-01-20 17:31:07 -0800

    render the form

--- src/containers/DietNew.js
+++ src/containers/DietNew.js
@@ -32,7 +32,44 @@ export class DietNew extends Component {
 
     render() {
         return (
-          <div>
+            <div class="wrapper">
+                <form onSubmit={this.handleSubmit}>
+                    <h3 className="center">ADD YOUR DIET </h3>
+                    <div className="input-field">
+                        <input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} />
+                        <label htmlFor="name">Name</label>
+                    </div>
+                    <div className="input-field">
+                        <input type="text" name="age" id="age" value={this.state.age} onChange={this.handleChange} />
+                        <label htmlFor="age">Age</label>
+                    </div>
+                    <div className="input-field">
+                        <input type="text" name="gender" id="gender" value={this.state.gender} onChange={this.handleChange} />
+                        <label htmlFor="gender">Gender</label>
+                    </div>
+                    <div className="input-field">
+                        <input type="text" name="height" id="height" value={this.state.height} onChange={this.handleChange} />
+                        <label htmlFor="height">Height</label>
+                    </div>
+                    <div className="input-field">
+                        <input type="text" name="diet_type" id="diet_type" value={this.state.diet_type} onChange={this.handleChange} />
+                        <label htmlFor="diet_type">Diet Type</label>
+                    </div>
+                    <div className="input-field">
+                        <input type="text" name="start_weight" id="start_weight" value={this.state.start_weight} onChange={this.handleChange} />
+                        <label htmlFor="start_weight">Start Weight</label>
+                    </div>
+                    <div className="input-field">
+                        <input type="text" name="lost_weight" id="lost_weight" value={this.state.lost_weight} onChange={this.handleChange} />
+                        <label htmlFor="lost_weight">Weight Lost</label>
+                    </div>
+                    <div className="input-field">
+                        <input type="text" name="duration" id="duration" value={this.state.duration} onChange={this.handleChange} />
+                        <label htmlFor="duration">Duration</label>
+                    </div>
+
+                    <input type="submit" value="ADD TO THE LIST" className="btn" />
+                </form>
             </div>
         )
     }
